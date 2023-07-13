504b0248928422315262c71dc364d0f3944b4e92
treasureanele05 <treasureanele05@gmail.com>
2023-07-13 00:17:56 +0000

    module page populated with defined attributes

--- coursereg/views.py
+++ coursereg/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Module
 
 
 def home(request):
     return render(request, "coursereg/home.html", {"title": "Home page"})
+
+def modules(request):
+    return render(request, "coursereg/modules.html", {"title": "Modules page","modules":Module.objects.all()})
 
